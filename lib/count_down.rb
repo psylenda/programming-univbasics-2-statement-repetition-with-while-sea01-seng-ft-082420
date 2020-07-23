78e350fec9b514a9f13b14571ff9344ce408b7ae
Jonathan Hong <hongjonsilver@gmail.com>
2020-07-23 17:23:58 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,6 @@
 # Write your code here
 count = 10 
 while (count >= 0) do
-  if count = 0 
-    puts "${count}"
-    puts "Happy New Year!"
-    count -= 1
-  elsif
     puts "${count}"
     count -= 1
 end
